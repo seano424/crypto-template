87ecd13822a22fc32026e4f84f4219c77221d049
Sean OReilly <soreilly424@gmail.com>
2022-05-13 14:13:57 -0500

    Organized Home Page into components

---
+++ components/HomePage/BottomContent.tsx
@@ -0,0 +1,13 @@
+import CtaNumbers from '../CTAs/CtaNumbers'
+import CtaCreateAccountSmall from '../CTAs/CtaCreateAccountSmall'
+import CtaCreateAccountLarge from '../CTAs/CtaCreateAccountLarge'
+
+export default function BottomContent() {
+  return (
+    <>
+      <CtaNumbers />
+      <CtaCreateAccountSmall />
+      <CtaCreateAccountLarge />
+    </>
+  )
+}
---
+++ components/HomePage/MainContent.tsx
@@ -0,0 +1,21 @@
+import Hero from '../Hero'
+import CtaCardsWithLabelLarge from '../CTAs/CtaCardsWithLabelLarge'
+import AsFeaturedIn from '../Banners/AsFeaturedIn'
+import CtaCustomerCard from '../CTAs/CtaCustomerCard'
+import CtaJoinCommunity from '../CTAs/CtaJoinCommunity'
+import CtaMasonryGrid from '../CTAs/CtaMasonryGrid'
+import CtaGridWithImages from '../CTAs/CtaGridWithImages'
+
+export default function MainContent() {
+  return (
+    <div className="max-w-[1440px] mx-auto select-none">
+      <Hero />
+      <AsFeaturedIn />
+      <CtaCardsWithLabelLarge />
+      <CtaGridWithImages />
+      <CtaMasonryGrid />
+      <CtaCustomerCard />
+      <CtaJoinCommunity />
+    </div>
+  )
+}
